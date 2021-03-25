087829525bea66d1a6d4b263f219c0dcaaca6e32
Yusuke Sato <yusuke.sato@ys-office.me>
2021-03-26 07:37:26 +0900

    add an error handler to the Flask app inside app.py:

--- blog/app.py
+++ blog/app.py
@@ -1,9 +1,17 @@
 from flask import Flask, jsonify, request
+from pydantic import ValidationError
 
 from blog.commands import CreateArticleCommand
 from blog.queries import GetArticleByIDQuery, ListArticlesQuery
 
 app = Flask(__name__)
+
+
+@app.errorhandler(ValidationError)
+def handle_validation_exception(error):
+    response = jsonify(error.errors())
+    response.status_code = 400
+    return response
 
 
 @app.route('/create-article/', methods=['POST'])
